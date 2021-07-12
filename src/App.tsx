cb72e0785084b7f347565c3b5f6c3aaeb7c08724
joaomoschetta <joaopedromschneider123@gmail.com>
2021-07-12 16:57:43 -0300

    Added and setted react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// Pages
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
     <div>
-      <Home />
+      <BrowserRouter>
+        <Route 
+          path="/" 
+          exact 
+          component={ Home } 
+        />
+        <Route 
+          path="/rooms/new" 
+          component={ NewRoom } 
+        />
+      </BrowserRouter>
     </div>
   );
 }
