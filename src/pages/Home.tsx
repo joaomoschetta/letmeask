b9a8b2a9f3ba155a23a1b73c9af48b58aee975a9
joaomoschetta <joaopedromschneider123@gmail.com>
2021-07-16 20:31:27 -0300

    Added functionality to join in existing room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
 import { useHistory } from 'react-router';
+import { FormEvent, useState } from 'react';
+
+import { database } from '../services/firebase';
 
 // Images
 import illustrationImg from '../assets/images/illustration.svg';
@@ -15,6 +18,7 @@ import '../styles/auth.scss';
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [ roomCode, setRoomCode ] = useState('');
 
   async function handleCreateRoom() {
     if (!user) {
@@ -22,6 +26,23 @@ export function Home() {
     }
 
     history.push('rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -42,10 +63,12 @@ export function Home() {
 
           <div className="separator">ou entre em uma sala</div>
 
-          <form action="">
+          <form onSubmit={handleJoinRoom}>
             <input 
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
